667edf63227822b95bcd8703d7da41b749c00ae7
Ernest Buffington <ernest.buffington@gmail.com>
2023-05-11 13:27:03 -0400

    Create web.php

---
+++ protected/config/web.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * This file provides to overwrite the default an602 / Yii configuration by your local Web environments
+ * @see http://www.yiiframework.com/doc-2.0/guide-concept-configurations.html
+ * @see http://docs.an602.org/admin-installation-configuration.html
+ * @see http://docs.an602.org/dev-environment.html
+ */
+return [
+];
+
